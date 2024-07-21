90bda80bfcc1c0d6e903630981819112f9916a59
Haris <haris503@hotmail.com>
2024-07-21 00:56:00 -0400

    Make dialog submit names

--- script.js
+++ script.js
@@ -56,6 +56,7 @@ const gameboard = (function () {
 const gameController = (function () {
   let currentPlayer = 0;
   const symbols = ["X", "O"];
+  let playersNames = ["Player 1", "Player 2"];
 
   const getCurrentPlayerSymbol = () => symbols[currentPlayer];
 
@@ -64,7 +65,7 @@ const gameController = (function () {
       gameboard.place(position, getCurrentPlayerSymbol());
 
       if (gameboard.gameover() === "win") {
-        displayController.updateResult(`Player ${currentPlayer + 1} wins!`);
+        displayController.updateResult(`${playersNames[currentPlayer]} wins!`);
       } else if (gameboard.gameover() === "draw") {
         displayController.updateResult("It's a draw.");
       }
@@ -73,12 +74,17 @@ const gameController = (function () {
     }
   };
 
+  const setPlayersNames = (player1, player2) => {
+    if (player1 !== "") playersNames[0] = player1;
+    if (player2 !== "") playersNames[1] = player2;
+  };
+
   const reset = function () {
     currentPlayer = 0;
     gameboard.clear();
   };
 
-  return { playRound, getCurrentPlayerSymbol, reset };
+  return { playRound, getCurrentPlayerSymbol, setPlayersNames, reset };
 })();
 
 const displayController = (function () {
@@ -86,6 +92,10 @@ const displayController = (function () {
   const resultMessage = document.getElementById("result");
   const boardBtns = document.querySelectorAll("#board > button");
   const resetBtn = document.getElementById("reset");
+  const dialog = document.querySelector("dialog");
+  const startBtn = document.getElementById("start");
+
+  window.addEventListener("load", () => dialog.showModal(), { once: true });
 
   boardBtns.forEach((btn, index) => {
     btn.addEventListener("click", (event) => {
@@ -107,6 +117,14 @@ const displayController = (function () {
     });
     updateResult("");
   });
+
+  startBtn.addEventListener("click", () => {
+    const form = document.forms["start-form"];
+    const fd = new FormData(form);
+
+    gameController.setPlayersNames(fd.get("player1-name"), fd.get("player2-name"));
+    dialog.close();
+  }, { once: true });
 
   return { updateResult };
 })();
